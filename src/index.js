f6cb35a53f665dc2a21ef07473ee3a9b648ae501
itzaur <preneur85@gmail.com>
2022-12-16 15:09:13 +0300

    add circle slider

--- src/index.js
+++ src/index.js
@@ -9,19 +9,28 @@ const images = importAll(
   require.context("../src/images", false, /\.(png|jpe?g|gif|svg|webp)$/)
 );
 
-const frame = document.querySelector(".frame");
+const frame = document.querySelector(".slider");
 const content = document.querySelector(".content");
 const menuWrapper = document.querySelector(".menu-wrapper");
 const menuItems = document.querySelectorAll(".menu__item");
-console.log(menuItems);
+// console.log(menuItems);
 const openMenuBtn = document.querySelector(".button-menu");
 const closeMenuBtn = document.querySelector(".button-close");
 const overlayPath = document.querySelector(".overlay__path");
+
+//Slider
+const slider = document.querySelector(".slider");
+
+//Test
+const closeMenuBtns = document.querySelectorAll(".slider__link");
+
+////
 
 const title = {
   main: document.querySelector(".content__title-main"),
   down: document.querySelector(".content__title-down"),
   bg: document.querySelector(".content__title-bg"),
+  services: document.querySelector(".services__title"),
 };
 
 let isAnimating = false;
@@ -60,7 +69,7 @@ const openMenuAnimation = () => {
       },
     })
     .to(
-      [title.main, title.down, title.bg],
+      [title.main, title.down, title.bg, title.services, slider],
       {
         y: -200,
         duration: 0.8,
@@ -152,7 +161,7 @@ const closeMenuAnimation = () => {
       duration: 0.8,
     })
     .to(
-      [title.main, title.down, title.bg],
+      [title.main, title.down, title.bg, title.services, slider],
       {
         y: 0,
         duration: 1.2,
@@ -174,5 +183,107 @@ const closeMenuAnimation = () => {
     );
 };
 
-openMenuBtn.addEventListener("click", openMenuAnimation);
+// openMenuBtn.addEventListener("click", openMenuAnimation);
 closeMenuBtn.addEventListener("click", closeMenuAnimation);
+
+closeMenuBtns.forEach((btn) => {
+  btn.addEventListener("click", openMenuAnimation);
+});
+
+//ANCHOR Gallery
+const gallery = document.querySelector(".gallery");
+const galleryItem = document.querySelectorAll(".gallery__img");
+
+let isActive = false;
+
+gallery.addEventListener("click", (e) => {
+  isActive = !isActive;
+  const clicked = e.target.closest(".gallery__img");
+  // if (!clicked) return;
+
+  // if (!e.target.classList.contains("active")) {
+  //   galleryItem.forEach((item) => {
+  //     item.classList.remove("active");
+  //     console.log("remove active");
+  //   });
+
+  // }
+
+  // galleryItem.forEach((item) => item.classList.remove("active"));
+
+  if (isActive) {
+    clicked.classList.add("active");
+  } else {
+    clicked.classList.remove("active");
+    clicked.classList.add("not-active");
+    setTimeout(() => {
+      clicked.classList.remove("not-active");
+    }, 950);
+  }
+});
+
+// window.addEventListener("click", (e) => {
+//   if (!e.target.closest(".gallery")) {
+//     galleryItem.forEach((item) => {
+//       item.classList.remove("active");
+//       console.log("click");
+//     });
+//   }
+// });
+
+// const gallery = gsap.utils.toArray(".gallery");
+// const galleryItem = gsap.utils.toArray(".gallery img");
+// const galleryToggles = galleryItem.map(createAnimation);
+
+// galleryItem.forEach((item) => {
+//   item.addEventListener("click", () => toggleGallery(item));
+// });
+// function toggleGallery(clickedItem) {
+//   galleryToggles.forEach((toggleFn) => toggleFn(clickedItem));
+// }
+
+// // galleryItem.forEach((item) => {
+// //   item.addEventListener("click", () => toggleGallery(item));
+// // });
+
+// // function toggleGallery(clickedItem) {
+// //   galleryToggles.forEach((toggleFn) => toggleFn(clickedItem));
+// // }
+
+// function createAnimation(element) {
+//   // let item = document.querySelector("img");
+
+//   let animation = gsap
+//     .timeline({ paused: true })
+//     .to(element, {
+//       "clip-path": "circle(100% at 50% 70%)",
+//       "grid-area": "1 / 1 / -1 / -1",
+//       duration: 2,
+//       ease: "power4",
+//     })
+//     .reverse();
+
+//   return function (clickedItem) {
+//     if (clickedItem === element) {
+//       console.log(clickedItem, element);
+//       animation.reversed(!animation.reversed());
+//       element.classList.add("active");
+//     } else {
+//       console.log(clickedItem, element);
+//       animation.reverse();
+//       element.classList.remove("active");
+//     }
+//   };
+// }
+
+/*
+ * Slider
+ */
+// const servicesButton = document.querySelector(".services__title");
+const sliderItem = document.querySelector(".slider__list");
+let count = 0;
+
+title.services.addEventListener("click", (e) => {
+  count++;
+  sliderItem.style.rotate = `${count * -90}deg`;
+});
